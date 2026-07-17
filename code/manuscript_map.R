fb9244ce6315df7d1a3fa495a20c31c98ecc019e
Patrick Thompson <patrick.thompson@dfo-mpo.gc.ca>
2026-07-16 17:09:46 -0700

    updated the label sizes

--- code/manuscript_map.R
+++ code/manuscript_map.R
@@ -116,9 +116,9 @@ study_area <- ggplot() +
   geom_sf(data = osoyoos_lake, fill = "grey85", linewidth = 0.3) +
   geom_sf(data = border, color = "grey50", linewidth = 0.3)+
   geom_text(data = lake_labels, aes(x, y, label = label), fontface = "italic", size = 3)+
-  geom_sf(data = VDS13, pch = 19, size = 1.5)+
-  geom_sf(data = ECCC_station, pch = 19, size = 1.5)+
-  geom_sf(data = towns_sf, pch = 19, size = 1.5)+
+  geom_sf(data = VDS13, pch = 19, size = 2)+
+  geom_sf(data = ECCC_station, pch = 19, size = 2)+
+  geom_sf(data = towns_sf, pch = 19, size = 2)+
   geom_sf_text(data = towns_sf, aes(label = name, hjust = hjust), size = 2.2, nudge_x = towns_sf$nudge_x, nudge_y = towns_sf$nudge_y) +
   annotate("segment", x = 1469500,  xend = 1480000, y = y_top,  yend = y_top, linetype = 2,   linewidth = 0.3) +
   annotate("segment", x = 1469500,  xend = 1480000, y = y_bottom, yend = y_bottom, linetype = 2, linewidth = 0.3)+
